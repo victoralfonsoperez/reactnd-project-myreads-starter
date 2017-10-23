bd798380bc3c7e6773de26935608c6812e0da329
Victor Perez <vaperezr@unal.edu.co>
2017-10-22 21:15:28 -0500

    feat: create listBook component and implement app routing
    Delete state prop into BooksApp component, implements react roter
    dom into App.js, moves list-book markup into a single component,
    implements BrowseRouter into index.js to keep track of the app
    state by the browser URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter} from 'react-router-dom' // eslint-disable-line no-unused-vars
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter><App /></BrowserRouter>,
+    document.getElementById('root')
+)
